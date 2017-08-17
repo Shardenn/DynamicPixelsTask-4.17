18d6f3e6797f74c7926e304cdafe793c64b4bbcb
Shardenn <vickarchuck@yandex.ru>
2017-08-18 01:52:39 +0300

    Added bot classes with some set up stuff
    Added bots' controller and pawn. Set up controller a bit so it knows who
    the player is, what controller is controlling. Also set all default
    values in gameMode.

---
+++ Source/DynamicPixelsTask/Private/EnemyAIController.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../Public/EnemyAIController.h"
+void AEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("Possesing smth"));
+}
+
+void AEnemyAIController::SetPawn(APawn * OurPawn)
+{
+	Super::SetPawn(OurPawn);
+
+	if (OurPawn)
+	{
+		auto PossesedBot = Cast<AEnemyCharacter>(OurPawn);
+		if (!ensure(PossesedBot)) { return; }
+
+		//UE_LOG(LogTemp, Warning, TEXT("Possesed bot that has ID %s"), *(PossesedBot->GetName()))
+		
+	}
+	else
+	{
+
+	}
+}
+
+void AEnemyAIController::Tick(float deltaTime)
+{
+	Super::Tick(deltaTime);
+
+	auto PlayerActor = AActor::GetWorld()->GetFirstPlayerController()->GetPawn();
+	auto PossesedBot = AController::GetPawn();
+
+	ReachActor(PlayerActor, PossesedBot);
+
+	if (!(PlayerActor && PossesedBot)) { return;  }
+	
+	UE_LOG(LogTemp, Warning, TEXT("Possesed bot that has ID %s"), *(PossesedBot->GetName()));
+	UE_LOG(LogTemp, Warning, TEXT("Possesed bot that has ID %s"), *(PlayerActor->GetName()));
+	
+	UE_LOG(LogTemp, Warning, TEXT("Bot's location %s"), *PossesedBot->GetActorLocation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Player's location %s"), *PlayerActor->GetActorLocation().ToString());
+
+	
+}
+
+AActor* AEnemyAIController::FindPickup()
+{
+	/*for (TActorIterator<AStaticMeshActor*> ActorIt(AActor::GetWorld()); ActorIt; ++ActorIt)
+	{
+
+	}
+	*/
+	return NULL;
+}
+
+//void AEnemyAIController::SetFocus(AActor *)
+
+
+void AEnemyAIController::ReachActor(AActor* Target, APawn* Bot)
+{
+	AAIController::SetFocus(Target);
+	Bot->AddMovementInput(Bot->GetActorForwardVector(), 1.0f);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Inside reachActor method %s"), *(Bot->GetActorForwardVector().ToString()));
+}
+
---
+++ Source/DynamicPixelsTask/Public/EnemyAIController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Runtime/Engine/Classes/Engine/StaticMeshActor.h" // For our iterator
+#include "PickUp.h" // For finding our pickUp object by class as an actor
+#include "Runtime/Engine/Public/EngineUtils.h" // For TActorIterator to find pickUp. Too much for one simple method to include (
+#include "Runtime/Engine/Classes/GameFramework/Controller.h" // dont see SetPawn to override, lets try include
+#include "EnemyCharacter.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICPIXELSTASK_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+protected:
+	float AcceptableRadius = 250.0f;
+
+private:
+	virtual AActor* FindPickup();
+	//virtual void SetFocus(AActor*);
+
+	virtual void BeginPlay() override;
+	virtual void SetPawn(APawn* OurPawn) override;
+	virtual void Tick(float deltaTime) override;
+	virtual void ReachActor(AActor* ActorToReach, APawn* CurrentPawn);
+};
